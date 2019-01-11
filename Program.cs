d88b55e2a93f035d002577699c36e0b1267f2975
Deepak Acharya <aacharyadeepak100@gmail.com>
2019-01-11 15:23:40 +0530

    ref - read/write

--- Program.cs
+++ Program.cs
@@ -4,29 +4,31 @@ namespace play_with_passing_value_type_params
 {
     class Program
     {
+        public static void P(dynamic msg) => Console.WriteLine(msg);
+
         static void ChangeMadroNannana(int x, int y)
         {
-            x+=10;
-            y+=10;
+            x += 10;
+            y += 10;
         }
 
-        static void ChangeMadroNannanaOut(out int x, out int y)
+        static void ChangeMadroNannanaRef(ref int x, ref int y)
         {
-            x=10;
-            y=20;
-        } // Out *Update* - all out params needs to be updated before control leaves the function
-        public static void P(dynamic msg) => Console.WriteLine(msg);
+            P(x);
+            x = 10;
+            y = 20;
+        } // Ref *Read and Write* - Read/Write Permission
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            int x=1,y=1;
-            ChangeMadroNannana(x,y);
-            P("X = "+x);
-            P("Y = "+y);
+            int x = 1, y = 1;
+            ChangeMadroNannana(x, y);
+            P("X = " + x);
+            P("Y = " + y);
 
-            ChangeMadroNannanaOut(out x,out y);
-            P("X = "+x);
-            P("Y = "+y);
+            ChangeMadroNannanaRef(ref x, ref y);
+            P("X = " + x);
+            P("Y = " + y);
 
             //Output
 
@@ -34,9 +36,9 @@ namespace play_with_passing_value_type_params
             // Hello World!
             // X = 1
             // Y = 1
+            // 1
             // X = 10
             // Y = 20
-
         }
     }
 }
